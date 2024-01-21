5dd8265caf51d32a06a84e01e0511644d6ee067c
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-21 18:10:44 -0300

    style: starts the page style

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,47 @@
 <template>
-	<div id="app">
-		<input type="checkbox" id="theme-swith" v-model="darkMode">
-		<label for="theme-swith">
-			<span v-if="darkMode === true">
-				<p>Light</p>
-			</span>
-			<span v-else>
-				<p>Dark</p>
-			</span>
-		</label>
-	</div>
+    <div id="app">
+        <p>Olá, mundo!</p>
+    </div>
 </template>
 
 <script>
+// precast
 export default {
-	name: 'App',
-	data() {
-		return {
-			darkMode: false
-		};
-	},
-	mounted() {
-		let bodyElement = document.body;
-		bodyElement.classList.add('app-background');
+    name: 'App',
+    data() {
+        return {
+            darkMode: false
+        };
+    },
+    mounted() {
+        let bodyElement = document.body;
+        bodyElement.classList.add('app-container');
 
-		let htmlElement = document.documentElement;
-		let theme = localStorage.getItem('theme');
+        let htmlElement = document.documentElement;
+        let theme = localStorage.getItem('theme');
 
-		if (theme === 'dark') {
-			htmlElement.setAttribute('theme', 'dark');
+        if (theme === 'dark') {
+            htmlElement.setAttribute('theme', 'dark');
 
-			this.darkMode = true;
-		} else {
-			htmlElement.setAttribute('theme', 'light');
+            this.darkMode = true;
+        } else {
+            htmlElement.setAttribute('theme', 'light');
 
-			this.darkMode = false;
-		}
-	},
-	watch: {
-		darkMode: function () {
-			let htmlElement = document.documentElement;
+            this.darkMode = false;
+        }
+    },
+    watch: {
+        darkMode: function () {
+            let htmlElement = document.documentElement;
 
-			if (this.darkMode) {
-				localStorage.setItem('theme', 'dark');
-				htmlElement.setAttribute('theme', 'dark');
-			} else {
-				localStorage.setItem('theme', 'light');
-				htmlElement.setAttribute('theme', 'light');
-			}
-		}
-	}
+            if (this.darkMode) {
+                localStorage.setItem('theme', 'dark');
+                htmlElement.setAttribute('theme', 'dark');
+            } else {
+                localStorage.setItem('theme', 'light');
+                htmlElement.setAttribute('theme', 'light');
+            }
+        }
+    }
 }
 </script>
