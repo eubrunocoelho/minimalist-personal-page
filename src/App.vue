a204f0db279ea72a6f305d00a5d7b054624e5022
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-21 17:38:06 -0300

    feat: adds styling feature for light and dark themes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,54 @@
 <template>
-	<p>Olá, mundo!</p>
+    <div id="app">
+		<input type="checkbox" id='theme-switch' v-model="darkMode"/>
+		<label for='theme-switch'>
+			<span v-if="darkMode === true">
+				<p>Light</p>
+			</span>
+			<span v-else>
+				<p>Dark</p>
+			</span>
+		</label>
+    </div>
 </template>
 
 <script>
 export default {
 	name: 'App',
+	data() {
+		return {
+			darkMode: false
+		};
+	},
+	mounted() {
+		let bodyElement = document.body;
+		bodyElement.classList.add('app-background');
+
+		let htmlElement = document.documentElement;
+		let theme = localStorage.getItem('theme');
+
+		if (theme === 'dark') {
+			htmlElement.setAttribute('theme', 'dark');
+
+			this.darkMode = true;
+		} else {
+			htmlElement.setAttribute('theme', 'light');
+
+			this.darkMode = false;
+		}
+	},
+	watch: {
+		darkMode: function () {
+			let htmlElement = document.documentElement;
+
+			if (this.darkMode) {
+				localStorage.setItem('theme', 'dark');
+				htmlElement.setAttribute('theme', 'dark');
+			} else {
+				localStorage.setItem('theme', 'light');
+				htmlElement.setAttribute('theme', 'light');
+			}
+		}
+	}
 }
 </script>
