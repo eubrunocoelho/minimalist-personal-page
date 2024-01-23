ee4fad69944a468fa6a7fac92d125f8779e760e9
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-23 15:26:00 -0300

    feat: add fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import './assets/scss/styles.scss';
 
+library.add(fas, fab, far);
+dom.watch();
+
 const app = createApp(App);
-
 app.config.ignoredElements = [/^ion-/];
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
