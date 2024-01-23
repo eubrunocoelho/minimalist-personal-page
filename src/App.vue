db5337ad6621940cad33b25efa5cb5cd5dcbdf0d
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-23 15:04:15 -0300

    style: mobile: add style to footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
     <main id="app-container">
         <VueHeader></VueHeader>
-        <footer></footer>
+        <footer class="footer">
+            <nav class="footer__nav">
+                <div class="footer__nav__title">
+                    <h1 class="footer__nav__title__heading">Bruno Coelho</h1>
+                    <p class="footer__nav__title__description">Desenvolvedor Full-Stack</p>
+                </div>
+                <ul class="footer__nav__menu">
+                    <li>
+                        <a href="#" class="footer__nav__menu__link">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__nav__menu__link">Habilidades</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer__nav__menu__link">Contato</a>
+                    </li>
+                </ul>
+                <ul class="footer__nav__socials">
+                    <li class="footer__nav__socials__item">
+                        <a href="#" class="footer__nav__socials__link">#</a>
+                    </li>
+                    <li class="footer__nav__socials__item">
+                        <a href="#" class="footer__nav__socials__link">#</a>
+                    </li>
+                    <li class="footer__nav__socials__item">
+                        <a href="#" class="footer__nav__socials__link">#</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="footer__copy">
+                <p class="footer__copy__text">© Copyright 2024 - Desenvolvido por Bruno Coelho</p>
+            </div>
+        </footer>
     </main>
 </template>
 
