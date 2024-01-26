ed43b595872b1197ccbc79ec26ebc9da203baf68
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-26 14:23:06 -0300

    feat: add presentation component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
     <main id="app-container">
         <VueHeader></VueHeader>
-        <section class="section">
-            <div class="section__presentation">
-                <img src="./assets/img/profile-img.png" class="section__presentation__profile-img">
-                <div class="section__presentation__summary">
-                    <h1 class="section__presentation__summary__title">Olá, sou o Bruno</h1>
-                    <p class="section__presentation__summary__text">Desenvolvedor Full-Stack,<br /> criando <span class="text--assent-color">{</span> soluções para a web <span class="text--assent-color">}</span>.</p>
-                    <div class="section__presentation__summary__button-area">
-                        <button class="button button--icon">Contato<i class="fa-regular fa-paper-plane button__icon"></i></button>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <VuePresentation></VuePresentation>
         <VueFooter></VueFooter>
     </main>
 </template>
 
 <script>
 import VueHeader from './components/Header.vue';
+import VuePresentation from './components/Presentation.vue';
 import VueFooter from './components/Footer.vue';
 
 // precast
 export default {
     name: 'App',
-    components: { VueHeader, VueFooter },
+    components: { VueHeader, VuePresentation, VueFooter },
     data() {
         return {
             darkMode: false
