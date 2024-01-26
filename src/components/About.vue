de8650e6ed74a37c748fae4a323eae082395486c
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-26 20:07:24 -0300

    feat: add social media to about me content

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,24 @@
     <section class="section">
         <VueHeading></VueHeading>
         <div class="section__about">
-            <p class="section__about__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, debitis quo. Iste eos, perspiciatis aspernatur dignissimos cumque, non accusamus saepe vel voluptates unde deserunt vitae? Qui itaque beatae laudantium dolorem.</p>
+            <p class="section__about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <ul class="section__about__socials">
+                <li class="section__about__socials__item">
+                    <a href="#" class="section__about__socials__item__link">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                </li>
+                <li class="section__about__socials__item">
+                    <a href="#" class="section__about__socials__item__link">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </li>
+                <li class="section__about__socials__item">
+                    <a href="#" class="section__about__socials__item__link">
+                        <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
     </section>
 </template>
