e00a8ea3cbab8ae6d96e0cec0cbd8655fa41e8c5
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-26 17:46:48 -0300

    feat: add text content about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,9 @@
 <template>
     <section class="section">
         <VueHeading></VueHeading>
+        <div class="section__about">
+            <p class="section__about__text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla, debitis quo. Iste eos, perspiciatis aspernatur dignissimos cumque, non accusamus saepe vel voluptates unde deserunt vitae? Qui itaque beatae laudantium dolorem.</p>
+        </div>
     </section>
 </template>
 
