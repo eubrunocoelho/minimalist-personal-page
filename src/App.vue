aba0417c0866f57e5d12c29703846adb75ad0a52
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-26 14:51:16 -0300

    feat: add heading to content section

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
         <VueHeader></VueHeader>
         <main class="main">
             <VuePresentation></VuePresentation>
+            <section class="section">
+                <div class="section__heading">
+                    <h1 class="section__heading__title">Sobre Mim</h1>
+                    <p class="section__heading__description">Bruno Coelho</p>
+                </div>
+            </section>
         </main>
         <VueFooter></VueFooter>
     </div>
