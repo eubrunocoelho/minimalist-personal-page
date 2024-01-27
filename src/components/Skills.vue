810f7bfa5120832d694314923c6b42fadd4fab9a
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-27 18:30:56 -0300

    feat: adds content to the skills component

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,10 +10,46 @@
                             <i class="section__skills__category__item__heading__icon-inner icon--html"></i>
                         </span>
                         <h3 class="section__skills__category__item__heading__title">HTML5</h3>
-                        <span class="section__skills__category__item__heading__level">80%</span>
+                        <span class="section__skills__category__item__heading__level">85%</span>
                     </div>
                     <div class="section__skills__category__item__progress-bar">
-                        <div class="section__skills__category__item__progress-bar__loaded" style="width: 80% !important;"></div>
+                        <div class="section__skills__category__item__progress-bar__loaded" style="width: 85% !important;"></div>
+                    </div>
+                </div>
+                <div class="section__skills__category__item">
+                    <div class="section__skills__category__item__heading">
+                        <span class="section__skills__category__item__heading__icon">
+                            <i class="section__skills__category__item__heading__icon-inner icon--css"></i>
+                        </span>
+                        <h3 class="section__skills__category__item__heading__title">CSS3</h3>
+                        <span class="section__skills__category__item__heading__level">78%</span>
+                    </div>
+                    <div class="section__skills__category__item__progress-bar">
+                        <div class="section__skills__category__item__progress-bar__loaded" style="width: 78% !important;"></div>
+                    </div>
+                </div>
+                <div class="section__skills__category__item">
+                    <div class="section__skills__category__item__heading">
+                        <span class="section__skills__category__item__heading__icon">
+                            <i class="section__skills__category__item__heading__icon-inner icon--javascript"></i>
+                        </span>
+                        <h3 class="section__skills__category__item__heading__title">JavaScript</h3>
+                        <span class="section__skills__category__item__heading__level">59%</span>
+                    </div>
+                    <div class="section__skills__category__item__progress-bar">
+                        <div class="section__skills__category__item__progress-bar__loaded" style="width: 59% !important;"></div>
+                    </div>
+                </div>
+                <div class="section__skills__category__item">
+                    <div class="section__skills__category__item__heading">
+                        <span class="section__skills__category__item__heading__icon">
+                            <i class="section__skills__category__item__heading__icon-inner icon--typescript"></i>
+                        </span>
+                        <h3 class="section__skills__category__item__heading__title">TypeScript</h3>
+                        <span class="section__skills__category__item__heading__level">22%</span>
+                    </div>
+                    <div class="section__skills__category__item__progress-bar">
+                        <div class="section__skills__category__item__progress-bar__loaded" style="width: 22% !important;"></div>
                     </div>
                 </div>
             </div>
@@ -33,5 +69,17 @@ export default {
 <style scoped>
 .icon--html {
     background: url(../assets/img/icons/html.svg);
+}
+
+.icon--css {
+    background: url(../assets/img/icons/css.svg);
+}
+
+.icon--javascript {
+    background: url(../assets/img/icons/js.svg);
+}
+
+.icon--typescript {
+    background: url(../assets/img/icons/ts.svg);
 }
 </style>
