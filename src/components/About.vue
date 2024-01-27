0a5bd656cc4ececdfdb0e36d1e277f92e4c4e082
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-27 17:08:15 -0300

    feat: optimizes the heading component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="section">
-        <VueHeading></VueHeading>
+        <VueHeading title="Sobre Mim" description="Bruno Coelho"></VueHeading>
         <div class="section__about">
             <div class="section__about__summary">
                 <p class="section__about__summary__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="section__heading">
-        <h1 class="section__heading__title">Sobre Mim</h1>
-        <p class="section__heading__description">Bruno Coelho</p>
+        <h1 class="section__heading__title">{{ title }}</h1>
+        <p class="section__heading__description">{{ description }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'VueHeading'
+    name: 'VueHeading',
+    props: {
+        title: {
+            required: true,
+            type: String
+        },
+        description: {
+            required: true,
+            type: String
+        }
+    }
 };
 </script>
