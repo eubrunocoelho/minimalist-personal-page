ca93fab0debd26e1bd42ac2090739b01eca32917
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-27 18:22:28 -0300

    refactor: change style nomenclature

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,18 +2,18 @@
     <section class="section">
         <VueHeading title="Habilidades" description="Conhecimento Técnico"></VueHeading>
         <div class="section__skills">
-            <div class="section__skills__item">
-                <h2 class="section__skills__item__title">Front-End</h2>
-                <div class="section__skills__item__content">
-                    <div class="section__skills__item__content__heading">
-                        <span class="section__skills__item__content__heading__icon">
-                            <i class="section__skills__item__content__heading__icon-inner icon--html"></i>
+            <div class="section__skills__category">
+                <h2 class="section__skills__category__title">Front-End</h2>
+                <div class="section__skills__category__item">
+                    <div class="section__skills__category__item__heading">
+                        <span class="section__skills__category__item__heading__icon">
+                            <i class="section__skills__category__item__heading__icon-inner icon--html"></i>
                         </span>
-                        <h3 class="section__skills__item__content__heading__title">HTML5</h3>
-                        <span class="section__skills__item__content__heading__level">80%</span>
+                        <h3 class="section__skills__category__item__heading__title">HTML5</h3>
+                        <span class="section__skills__category__item__heading__level">80%</span>
                     </div>
-                    <div class="section__skills__item__content__progress-bar">
-                        <div class="section__skills__item__content__progress-bar__loaded" style="width: 80% !important;"></div>
+                    <div class="section__skills__category__item__progress-bar">
+                        <div class="section__skills__category__item__progress-bar__loaded" style="width: 80% !important;"></div>
                     </div>
                 </div>
             </div>
