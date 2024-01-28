7c4118bb17450048d5beda3a3c20878c24fd3dde
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-01-28 10:49:13 -0300

    style: mobile: add style to contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,20 @@
 <template>
     <section class="section">
         <VueHeading title="Contato" description="Entre em Contato"></VueHeading>
+        <div class="section__contact">
+            <div class="section__contact__form">
+                <form action="">
+                    <input type="text" class="section__contact__form__input-text" placeholder="Digite seu nome...">
+                    <input type="text" class="section__contact__form__input-text" placeholder="Digite seu telefone/whatsapp...">
+                    <input type="text" class="section__contact__form__input-text" placeholder="Digite seu endereço de e-mail...">
+                    <input type="text" class="section__contact__form__input-text" placeholder="Digite o assunto...">
+                    <textarea class="section__contact__form__textarea" placeholder="Escreva sua mensagem..."></textarea>
+                    <div class="section__contact__form__button-area">
+                        <button class="button button--icon">Enviar<i class="fa-regular fa-paper-plane button__icon"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </section>
 </template>
 
