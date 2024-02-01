987508f8340053313b29871a1a0e1f9d180fcb6a
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-01 14:00:48 -0300

    refactor: change header style naming

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,28 +2,28 @@
     <header class="header">
         <div class="header-container">
             <div class="header__toggle-box">
-                <span class="header__toggle-box__line"></span>
-                <span class="header__toggle-box__line"></span>
-                <span class="header__toggle-box__line"></span>
+                <span class="toggle-box__line"></span>
+                <span class="toggle-box__line"></span>
+                <span class="toggle-box__line"></span>
             </div>
             <a href="#" class="header__logo">Bruno Coelho</a>
             <nav class="header__nav">
-                <ul class="header__nav__menu">
+                <ul class="header__menu">
                     <li>
-                        <a href="#" class="header__nav__menu__link">Home</a>
+                        <a href="#" class="header__link">Home</a>
                     </li>
                     <li>
-                        <a href="#" class="header__nav__menu__link">Sobre</a>
+                        <a href="#" class="header__link">Sobre</a>
                     </li>
                     <li>
-                        <a href="#" class="header__nav__menu__link">Habilidades</a>
+                        <a href="#" class="header__link">Habilidades</a>
                     </li>
                     <li>
-                        <a href="#" class="header__nav__menu__link">Contato</a>
+                        <a href="#" class="header__link">Contato</a>
                     </li>
                 </ul>
-                <div class="header__nav__theme-switch">
-                    <ion-icon name="sunny" class="header__nav__theme-switch__icon"></ion-icon>
+                <div class="header__theme-switch">
+                    <ion-icon name="sunny" class="theme-switch__icon"></ion-icon>
                 </div>
             </nav>
         </div>
