eae7cd446c374d1fd704a904ee6c1cb9cc77116a
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-01 14:17:17 -0300

    refactor: change presentation style naming

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="section">
         <div class="section__presentation">
-            <img src="./../assets/img/profile-img.png" class="section__presentation__profile-img">
-            <div class="section__presentation__summary">
-                <h1 class="section__presentation__summary__title">Olá, sou o Bruno</h1>
-                <p class="section__presentation__summary__text">Desenvolvedor Full-Stack,<br /> criando <span class="text--assent-color">{</span> soluções para a web <span class="text--assent-color">}</span>.</p>
-                <div class="section__presentation__summary__button-area">
+            <img src="./../assets/img/profile-img.png" class="presentation__profile-img">
+            <div class="presentation__summary">
+                <h1 class="presentation__title">Olá, sou o Bruno</h1>
+                <p class="presentation__text">Desenvolvedor Full-Stack,<br /> criando <span class="text--assent-color">{</span> soluções para a web <span class="text--assent-color">}</span>.</p>
+                <div class="presentation__button-area">
                     <button class="button button--icon">Contato<i class="fa-regular fa-paper-plane button__icon"></i></button>
                 </div>
             </div>
