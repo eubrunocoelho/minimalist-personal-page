28af9766640bf214d2c117ddfdd95cb7e7038864
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-03 14:26:44 -0300

    feat: adds feature to change themes

--- src/App.vue
+++ src/App.vue
@@ -19,44 +19,8 @@ import VueSkills from './components/Skills.vue';
 import VueContact from './components/Contact.vue';
 import VueFooter from './components/Footer.vue';
 
-// precast
 export default {
     name: 'App',
-    components: { VueHeader, VuePresentation, VueAbout, VueSkills, VueContact, VueFooter },
-    data() {
-        return {
-            darkMode: false
-        };
-    },
-    mounted() {
-        let bodyElement = document.body;
-        bodyElement.classList.add('app-container');
-
-        let htmlElement = document.documentElement;
-        let theme = localStorage.getItem('theme');
-
-        if (theme === 'dark') {
-            htmlElement.setAttribute('theme', 'dark');
-
-            this.darkMode = true;
-        } else {
-            htmlElement.setAttribute('theme', 'light');
-
-            this.darkMode = false;
-        }
-    },
-    watch: {
-        darkMode: function () {
-            let htmlElement = document.documentElement;
-
-            if (this.darkMode) {
-                localStorage.setItem('theme', 'dark');
-                htmlElement.setAttribute('theme', 'dark');
-            } else {
-                localStorage.setItem('theme', 'light');
-                htmlElement.setAttribute('theme', 'light');
-            }
-        }
-    }
+    components: { VueHeader, VuePresentation, VueAbout, VueSkills, VueContact, VueFooter }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,8 +22,9 @@
                         <a href="#" class="header__link">Contato</a>
                     </li>
                 </ul>
-                <div class="header__theme-switch">
-                    <ion-icon name="sunny" class="theme-switch__icon"></ion-icon>
+                <div class="header__theme-switch" @click="switchTheme">
+                    <ion-icon name="sunny" class="theme-switch__icon" v-if="darkMode === false"></ion-icon>
+                    <ion-icon name="moon" class="theme-switch__icon" v-else></ion-icon>
                 </div>
             </nav>
         </div>
@@ -32,6 +33,42 @@
 
 <script>
 export default {
-    name: 'VueHeader'
+    name: 'VueHeader',
+    data() {
+        return {
+            darkMode: false
+        };
+    },
+    mounted() {
+        let htmlElement = document.documentElement;
+        let theme = localStorage.getItem('theme');
+
+        if (theme === 'light') {
+            htmlElement.setAttribute('theme', 'light');
+
+            this.darkMode = false;
+        } else {
+            htmlElement.setAttribute('theme', 'dark');
+            
+            this.darkMode = true;
+        }
+    },
+    methods: {
+        switchTheme() {
+			let htmlElement = document.documentElement;
+
+            if (!this.darkMode) {
+				localStorage.setItem('theme', 'dark');
+                htmlElement.setAttribute('theme', 'dark');
+
+                this.darkMode = true;
+            } else {
+				localStorage.setItem('theme', 'light');
+                htmlElement.setAttribute('theme', 'light');
+
+                this.darkMode = false;
+            }
+        }
+    }
 };
 </script>
