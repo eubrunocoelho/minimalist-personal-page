6729828131faa72d2f5cd9e99e66bc1ab4c98472
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-06 11:27:35 -0300

    feat: precast form validation

--- src/components/Contact/Form.vue
+++ src/components/Contact/Form.vue
@@ -22,7 +22,7 @@
 
 <script>
 import useVuelidate from '@vuelidate/core';
-import { required } from '@vuelidate/validators';
+import { helpers, required, minLength, maxLength, email } from '@vuelidate/validators';
 
 export default {
     name: 'VueForm',
@@ -45,11 +45,43 @@ export default {
     validations() {
         return {
             form: {
-                name: { required },
-                whatsapp: { required },
-                email: { required },
-                subject: { required },
-                message: { required },
+                name: { 
+                    required: helpers.withMessage('O campo "nome" é obrigatório.', required),
+                    minLength: helpers.withMessage(
+                        ({ $params }) => `O campo "nome" deve conter pelo menos ${$params.min} caracteres.`, minLength(3)
+                    ),
+                    maxLength: helpers.withMessage(
+                        ({ $params }) => `O campo "nome" deve conter no máximo ${$params.max} caracteres.`, maxLength(128)
+                    )
+                },
+                whatsapp: {
+                    required: helpers.withMessage('O campo "telefone" é obrigatório.', required)
+                },
+                email: { 
+                    required: helpers.withMessage('O campo "e-mail" é obrigatório.', required),
+                    email: helpers.withMessage('O campo "e-mail" está inválido.', email),
+                    maxLength: helpers.withMessage(
+                        ({ $params }) => `O campo "e-mail" deve conter no máximo ${$params.max} caracteres.`, maxLength(128)
+                    )
+                },
+                subject: {
+                    required: helpers.withMessage('O campo "assunto" é obrigatório.', required),
+                    minLength: helpers.withMessage(
+                        ({ $params }) => `O campo "assunto" deve conter pelo menos ${$params.min} caracteres.`, minLength(12)
+                    ),
+                    maxLength: helpers.withMessage(
+                        ({ $params }) => `O campo "assunto" deve conter no máximo ${$params.max} caracteres.`, maxLength(128)
+                    )
+                },
+                message: {
+                    required: helpers.withMessage('O campo "mensagem" é obrigatório.', required),
+                    minLength: helpers.withMessage(
+                        ({ $params }) => `O campo "mensagem" deve conter pelo menos ${$params.min} caracteres.`, minLength(20)
+                    ),
+                    maxLength: helpers.withMessage(
+                        ({ $params }) => `O campo "mensagem" deve conter no máximo ${$params.max} caracteres.`, maxLength(1200)
+                    )
+                },
             }
         }
     },
