1e3dbf4842a429ac2a7b55748e496ceb33fa6b64
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-08 18:57:34 -0300

    refactor: create function to reduce the code

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -31,92 +31,91 @@ export default {
     components: { VueHeading, VueSkill },
     data() {
         return {
-            beforeLevel: 1,
             frontend: {
                 items: [
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'HTML5',
                         classIcon: 'icon--html',
-                        level: '85'
+                        level: 85
                     },
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'CSS3',
                         classIcon: 'icon--css',
-                        level: '73'
+                        level: 75
                     },
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'JavaScript',
                         classIcon: 'icon--javascript',
-                        level: '64'
+                        level: 69
                     },
                     {
                         beforeLevel: '1',
                         name: 'TypeScript',
                         classIcon: 'icon--typescript',
-                        level: '14'
+                        level: 17
                     }
                 ]
             },
             backend: {
                 items: [
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'PHP',
                         classIcon: 'icon--php',
-                        level: '69'
+                        level: 72
                     },
                     {
                         beforeLevel: '1',
                         name: 'Node.js',
                         classIcon: 'icon--nodejs',
-                        level: '35'
+                        level: 35
                     },
                     {
                         beforeLevel: '1',
                         name: 'Java',
                         classIcon: 'icon--java',
-                        level: '13'
+                        level: 13
                     }
                 ]
             },
             database: {
                 items: [
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'MySQL',
                         classIcon: 'icon--mysql',
-                        level: '49'
+                        level: 49
                     },
                     {
                         beforeLevel: '1',
                         name: 'SQL Server',
                         classIcon: 'icon--sqlserver',
-                        level: '35'
+                        level: 36
                     }
                 ]
             },
             frameworks: {
                 items: [
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'Laravel',
                         classIcon: 'icon--laravel',
-                        level: '39'
+                        level: 42
                     },
                     {
-                        beforeLevel: '1',
+                        beforeLevel: 1,
                         name: 'Vue.js',
                         classIcon: 'icon--vuejs',
-                        level: '45'
+                        level: 47
                     },
                     {
                         beforeLevel: '1',
                         name: 'Express.js',
                         classIcon: 'icon--expressjs',
-                        level: '31'
+                        level: 31
                     }
                 ]
             }
@@ -143,35 +142,33 @@ export default {
                 items['frameworks']
             ) {
                 const
-                    frontendPosition = (items['frontend'].getBoundingClientRect().top + window.scrollY) + 120,
-                    backendPosition = (items['backend'].getBoundingClientRect().top + window.scrollY) + 120,
-                    databasePosistion = (items['database'].getBoundingClientRect().top + window.scrollY) + 120,
-                    frameworksPosition = (items['frameworks'].getBoundingClientRect().top + window.scrollY) + 120;
+                    frontendPosition = (items['frontend'].getBoundingClientRect().top + window.scrollY) + 180,
+                    backendPosition = (items['backend'].getBoundingClientRect().top + window.scrollY) + 180,
+                    databasePosistion = (items['database'].getBoundingClientRect().top + window.scrollY) + 180,
+                    frameworksPosition = (items['frameworks'].getBoundingClientRect().top + window.scrollY) + 180;
 
                 if (visibleHeight > frontendPosition) {
-                    this.frontend.items.forEach(item => {
-                        item.beforeLevel = item.level;
-                    });
+                    this.skillAnimation(this.frontend.items);
                 }
 
                 if (visibleHeight > backendPosition) {
-                    this.backend.items.forEach(item => {
-                        item.beforeLevel = item.level;
-                    });
+                    this.skillAnimation(this.backend.items);
                 }
 
                 if (visibleHeight > databasePosistion) {
-                    this.database.items.forEach(item => {
-                        item.beforeLevel = item.level;
-                    });
+                    this.skillAnimation(this.database.items);
                 }
 
                 if (visibleHeight > frameworksPosition) {
-                    this.frameworks.items.forEach(item => {
-                        item.beforeLevel = item.level;
-                    });
+                    this.skillAnimation(this.frameworks.items);
                 }
             }
+        },
+
+        skillAnimation(item) {
+            item.forEach(item => {
+                item.beforeLevel = item.level;
+            });
         }
     }
 };
