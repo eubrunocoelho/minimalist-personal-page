8ee5da0035bb378371958c3d4b37235ec5821aaf
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-09 11:21:40 -0300

    feat: add typewriter animation in presentation component

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -4,7 +4,7 @@
             <img src="./../assets/img/profile-img.png" class="presentation__profile-img">
             <div class="presentation__summary">
                 <h1 class="presentation__title">Olá, sou o Bruno</h1>
-                <p class="presentation__text">Desenvolvedor Full-Stack,<br /> criando <span class="text--assent-color">{</span> <span>soluções para a web</span> <span class="text--assent-color">}</span>.</p>
+                <p class="presentation__text">Desenvolvedor Full-Stack,<br /> criando <span class="text--assent-color">{</span> <VueWriter :array="['soluções para a web']" :typeSpeed="99" :iterations="1" :style="{ display: 'inline' }" /> <span class="text--assent-color">}</span>.</p>
                 <div class="presentation__button-area">
                     <button class="button button--icon">Contato</button>
                 </div>
@@ -18,3 +18,9 @@ export default {
     name: 'VuePresentation'
 };
 </script>
+
+<style>
+span.cursor {
+    display: none !important;
+}
+</style>
