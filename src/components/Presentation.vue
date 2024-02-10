1665a82fafc8756e0c9529aa009f568fb3d48098
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-10 13:31:08 -0300

    chore: fix non-existent class

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -6,7 +6,7 @@
                 <h1 class="presentation__title">Olá, sou o Bruno</h1>
                 <p class="presentation__text">Desenvolvedor Full-Stack,<br /> criando <span class="text--assent-color">{</span> <VueWriter :array="['soluções para a web']" :typeSpeed="99" :iterations="1" :style="{ display: 'inline' }"></VueWriter> <span class="text--assent-color">}</span>.</p>
                 <div class="presentation__button-area">
-                    <button class="button button--icon">Contato</button>
+                    <button class="button">Contato</button>
                 </div>
             </div>
         </div>
