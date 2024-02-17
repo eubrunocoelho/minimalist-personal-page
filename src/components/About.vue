0454847f7cfda6343081374226162b2d48c5ef9e
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-17 16:23:47 -0300

    feat: creates component for socials

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,34 +3,32 @@
         <VueHeading title="Sobre Mim" subtitle="Bruno Coelho"></VueHeading>
         <div class="section__about">
             <div class="about__summary">
-                <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                    in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
-            <ul class="about__socials">
-                <li class="about__socials-item">
-                    <a href="#" class="about__socials-link">
-                        <i class="fa-brands fa-linkedin"></i>
-                    </a>
-                </li>
-                <li class="about__socials-item">
-                    <a href="#" class="about__socials-link">
-                        <i class="fa-brands fa-github"></i>
-                    </a>
-                </li>
-                <li class="about__socials-item">
-                    <a href="#" class="about__socials-link">
-                        <i class="fa-brands fa-whatsapp"></i>
-                    </a>
-                </li>
-            </ul>
+            <VueSocials :socials="socials"></VueSocials>
         </div>
     </section>
 </template>
 
 <script>
 import VueHeading from './Common/Heading.vue';
+import VueSocials from './About/Socials.vue';
 
 export default {
     name: 'VueAbout',
-    components: { VueHeading }
+    components: { VueHeading, VueSocials },
+    data() {
+        return {
+            socials: {
+                linkedin: { url: 'https://www.linkedin.com/in/eubrunocoelho', classIcon: 'fa-brands fa-linkedin' },
+                github: { url: 'https://github.com/eubrunocoelho', classIcon: 'fa-brands fa-github' },
+                whatsapp: { url: 'https://wa.me/5541987389397', classIcon: 'fa-brands fa-whatsapp' }
+            }
+        }
+    }
 };
 </script>
---
+++ src/components/About/Socials.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="about__socials">
+        <li class="about__socials-item" v-for="(item, id) in socials" :key="id">
+            <a :href="item.url" target="_blank" class="about__socials-link">
+                <i :class="item.classIcon"></i>
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'VueSocials',
+    props: {
+        socials: {
+            required: true,
+            type: Object
+        }
+    },
+    mounted() {
+        console.log(this.socials);
+    }
+}
+</script>
