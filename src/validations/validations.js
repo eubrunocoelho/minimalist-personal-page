9bd40b7b3a54c1dc3a8f5bfe943c36487b9d87c5
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-18 14:31:04 -0300

    chore: indent the code

--- src/validations/validations.js
+++ src/validations/validations.js
@@ -18,31 +18,75 @@ const errorMessages = {
     phoneFormat: (field) => `O formato de "${field}" deve ser (DD) 00000-0000 ou (DD) 0000-0000.`
 };
 
-export const validations = {
+const validations = {
     form: {
         name: {
-            required: withCustomMessage(required, () => errorMessages.required('nome')),
-            minLength: withCustomMessage(minLength(MIN_LENGTH_NAME), () => errorMessages.minLength('nome', MIN_LENGTH_NAME)),
-            maxLength: withCustomMessage(maxLength(MAX_LENGTH_NAME), () => errorMessages.maxLength('nome', MAX_LENGTH_NAME))
+            required: withCustomMessage(
+                required,
+                () => errorMessages.required('nome')
+            ),
+            minLength: withCustomMessage(
+                minLength(MIN_LENGTH_NAME),
+                () => errorMessages.minLength('nome', MIN_LENGTH_NAME)
+            ),
+            maxLength: withCustomMessage(
+                maxLength(MAX_LENGTH_NAME),
+                () => errorMessages.maxLength('nome', MAX_LENGTH_NAME)
+            )
         },
         phone: {
-            required: withCustomMessage(required, () => errorMessages.required('telefone')),
-            regex: withCustomMessage(helpers.regex(/^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}$/), () => errorMessages.phoneFormat('telefone'))
+            required: withCustomMessage(
+                required,
+                () => errorMessages.required('telefone')
+            ),
+            regex: withCustomMessage(
+                helpers.regex(/^\([0-9]{2}\) [0-9]?[0-9]{4}-[0-9]{4}$/),
+                () => errorMessages.phoneFormat('telefone')
+            )
         },
         email: {
-            required: withCustomMessage(required, () => errorMessages.required('e-mail')),
-            email: withCustomMessage(email, () => errorMessages.email('e-mail')),
-            maxLength: withCustomMessage(maxLength(MAX_LENGTH_EMAIL), () => errorMessages.maxLength('e-mail', MAX_LENGTH_EMAIL))
+            required: withCustomMessage(
+                required,
+                () => errorMessages.required('e-mail')
+            ),
+            email: withCustomMessage(
+                email,
+                () => errorMessages.email('e-mail')
+            ),
+            maxLength: withCustomMessage(
+                maxLength(MAX_LENGTH_EMAIL),
+                () => errorMessages.maxLength('e-mail', MAX_LENGTH_EMAIL)
+            )
         },
         subject: {
-            required: withCustomMessage(required, () => errorMessages.required('assunto')),
-            minLength: withCustomMessage(minLength(MIN_LENGTH_SUBJECT), () => errorMessages.minLength('assunto', MIN_LENGTH_SUBJECT)),
-            maxLength: withCustomMessage(maxLength(MAX_LENGTH_SUBJECT), () => errorMessages.maxLength('assunto', MAX_LENGTH_SUBJECT))
+            required: withCustomMessage(
+                required,
+                () => errorMessages.required('assunto')
+            ),
+            minLength: withCustomMessage(
+                minLength(MIN_LENGTH_SUBJECT),
+                () => errorMessages.minLength('assunto', MIN_LENGTH_SUBJECT)
+            ),
+            maxLength: withCustomMessage(
+                maxLength(MAX_LENGTH_SUBJECT),
+                () => errorMessages.maxLength('assunto', MAX_LENGTH_SUBJECT)
+            )
         },
         message: {
-            required: withCustomMessage(required, () => errorMessages.required('mensagem')),
-            minLength: withCustomMessage(minLength(MIN_LENGTH_MESSAGE), () => errorMessages.minLength('mensagem', MIN_LENGTH_MESSAGE)),
-            maxLength: withCustomMessage(maxLength(MAX_LENGTH_MESSAGE), () => errorMessages.maxLength('mensagem', MAX_LENGTH_MESSAGE))
+            required: withCustomMessage(
+                required,
+                () => errorMessages.required('mensagem')
+            ),
+            minLength: withCustomMessage(
+                minLength(MIN_LENGTH_MESSAGE),
+                () => errorMessages.minLength('mensagem', MIN_LENGTH_MESSAGE)
+            ),
+            maxLength: withCustomMessage(
+                maxLength(MAX_LENGTH_MESSAGE),
+                () => errorMessages.maxLength('mensagem', MAX_LENGTH_MESSAGE)
+            )
         }
     }
 };
+
+export { validations };
