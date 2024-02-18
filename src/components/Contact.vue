97ecf3c3888284d42f62d886e134e9ab56ba4c0e
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-18 16:18:05 -0300

    docs: remove docs

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,6 @@
         <div class="section__contact">
             <VueForm></VueForm>
             <div class="contact__information">
-                <!-- WHATSAPP -->
                 <div class="contact__data">
                     <span class="contact__icon"><i class="fa-brands fa-whatsapp"></i></span>
                     <div class="contact__description">
@@ -12,8 +11,6 @@
                         <p class="contact__subtitle">(41) 98738-9397</p>
                     </div>
                 </div>
-                <!-- ENDS WHATSAPP -->
-                <!-- E-MAIL -->
                 <div class="contact__data">
                     <span class="contact__icon"><i class="fa-regular fa-envelope"></i></span>
                     <div class="contact__description">
@@ -21,8 +18,6 @@
                         <p class="contact__subtitle">eu.brunocoelho94@gmail.com</p>
                     </div>
                 </div>
-                <!-- ENDS E-MAIL -->
-                <!-- LOCATION -->
                 <div class="contact__data">
                     <span class="contact__icon"><i class="fa-solid fa-location-dot"></i></span>
                     <div class="contact__description">
@@ -30,7 +25,6 @@
                         <p class="contact__subtitle">São José dos Pinhais/PR - Brasil</p>
                     </div>
                 </div>
-                <!-- END LOCATION -->
             </div>
         </div>
     </section>
