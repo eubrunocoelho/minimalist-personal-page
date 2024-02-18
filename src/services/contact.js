a3a591c58d154c8af27497a7e1ef7f51399ca371
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-18 14:30:13 -0300

    chore: changes nomenclature

--- src/services/contact.js
+++
@@ -1,7 +0,0 @@
-import { http } from './settings';
-
-export default {
-    send: ({ name, phone, email, subject, message }) => {
-        return http.post('send', { name, phone, email, subject, message });
-    }
-}
