252ac054b7d4ec053430df4170bb3800021fce29
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-18 16:25:12 -0300

    feat: create new component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,29 +3,7 @@
         <VueHeading title="Contato" subtitle="Entre em Contato"></VueHeading>
         <div class="section__contact">
             <VueForm></VueForm>
-            <div class="contact__information">
-                <div class="contact__data">
-                    <span class="contact__icon"><i class="fa-brands fa-whatsapp"></i></span>
-                    <div class="contact__description">
-                        <h2 class="contact__title">WhatsApp</h2>
-                        <p class="contact__subtitle">(41) 98738-9397</p>
-                    </div>
-                </div>
-                <div class="contact__data">
-                    <span class="contact__icon"><i class="fa-regular fa-envelope"></i></span>
-                    <div class="contact__description">
-                        <h2 class="contact__title">E-mail</h2>
-                        <p class="contact__subtitle">eu.brunocoelho94@gmail.com</p>
-                    </div>
-                </div>
-                <div class="contact__data">
-                    <span class="contact__icon"><i class="fa-solid fa-location-dot"></i></span>
-                    <div class="contact__description">
-                        <h2 class="contact__title">Localização</h2>
-                        <p class="contact__subtitle">São José dos Pinhais/PR - Brasil</p>
-                    </div>
-                </div>
-            </div>
+            <VueInformation :items="items"></VueInformation>
         </div>
     </section>
 </template>
@@ -33,9 +11,19 @@
 <script>
 import VueHeading from './Common/Heading.vue';
 import VueForm from './Contact/Form.vue';
+import VueInformation from './Contact/Information.vue';
 
 export default {
     name: 'VueContact',
-    components: { VueHeading, VueForm }
+    components: { VueHeading, VueForm, VueInformation },
+    data() {
+        return {
+            items: {
+                whatsapp: `(41) 98738-9397`,
+                email: `eu.brunocoelho94@gmail.com`,
+                location: `São José dos Pinhais/PR - Brasil`
+            }
+        }
+    }
 }
 </script>
---
+++ src/components/Contact/Information.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="contact__information">
+        <div class="contact__data">
+            <span class="contact__icon"><i class="fa-brands fa-whatsapp"></i></span>
+            <div class="contact__description">
+                <h2 class="contact__title">WhatsApp</h2>
+                <p class="contact__subtitle">{{ items.whatsapp }}</p>
+            </div>
+        </div>
+        <div class="contact__data">
+            <span class="contact__icon"><i class="fa-regular fa-envelope"></i></span>
+            <div class="contact__description">
+                <h2 class="contact__title">E-mail</h2>
+                <p class="contact__subtitle">{{ items.email }}</p>
+            </div>
+        </div>
+        <div class="contact__data">
+            <span class="contact__icon"><i class="fa-solid fa-location-dot"></i></span>
+            <div class="contact__description">
+                <h2 class="contact__title">Localização</h2>
+                <p class="contact__subtitle">{{ items.location }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueInformation',
+    props: {
+        items: {
+            required: true,
+            type: Object
+        }
+    }
+};
+</script>
