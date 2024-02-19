8334e250efc0d3f562dd8471cb420f389517b1f7
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-02-19 13:36:45 -0300

    style: changes the transition to linear

--- src/components/Skill/Skill.vue
+++ src/components/Skill/Skill.vue
@@ -8,7 +8,7 @@
             <span class="skills__level">{{ item.level + '%' }}</span>
         </div>
         <div class="skills__progress-bar">
-            <div class="skills__loaded" :style="{ width: item.beforeLevel + '%', important: true, transition: 'width 0.6s ease-in' }"></div>
+            <div class="skills__loaded" :style="{ width: item.beforeLevel + '%', important: true, transition: 'width 0.6s linear' }"></div>
         </div>
     </div>
 </template>
